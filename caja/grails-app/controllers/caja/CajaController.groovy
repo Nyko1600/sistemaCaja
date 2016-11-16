8bd72d23ab23984a5689716ccaf9b388c16c083e
nicolasCarballo <nicolas.carballop@gmail.com>
2016-11-16 12:16:24 -0300

    creamos los servicios

--- caja/grails-app/controllers/caja/CajaController.groovy
+++ caja/grails-app/controllers/caja/CajaController.groovy
@@ -1,6 +1,18 @@
 package caja
-
 class CajaController {
+	static scaffold = true
 
-    def index() { }
+	def cajaService
+	def index() {
+		
+	}
+	def home() {
+		def algo = cajaService.setData()
+	}
+
+	def caja(){
+		cajaService.addPay(1, 0,100)
+		cajaService.addPay(1, 10, -50)
+		cajaService.addPay(1, 20, -43)
+	}
 }
---
+++ caja/grails-app/domain/caja/Cliente.groovy
@@ -0,0 +1,11 @@
+package caja
+
+class Cliente {
+	int client_id
+	String nombre
+	String apellido
+	int doc
+	double saldo
+    static constraints = {
+    }
+}
